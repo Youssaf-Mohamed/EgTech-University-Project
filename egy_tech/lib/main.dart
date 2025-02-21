d21793b8a08be7ab3020341409135164a0e6d3f9
omar algamel <123903790+algamelomer@users.noreply.github.com>
2025-02-22 00:41:02 +0200

    splash screen added

--- egy_tech/lib/main.dart
+++ egy_tech/lib/main.dart
@@ -1,19 +1,21 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:my_app/screens/auth_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:provider/provider.dart';
 import 'repositories/auth_repository.dart';
 import 'screens/login_screen.dart';
 import 'screens/register_screen.dart';
-import 'screens/walkthrough.dart'; // Import the updated walkthrough file
+import 'screens/walkthrough.dart';
+// import 'screens/user_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Check walkthrough status
   final prefs = await SharedPreferences.getInstance();
-  // final bool walkthroughCompleted = prefs.getBool('walkthroughCompleted') ?? false;
-  final bool walkthroughCompleted = false; // for debugging
+  final bool walkthroughCompleted = false; // Debugging: Always show walkthrough
+
   runApp(
     MultiProvider(
       providers: [
@@ -37,13 +39,33 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
-      initialRoute: walkthroughCompleted ? '/' : '/walkthrough',
+      home: SplashScreen(walkthroughCompleted: walkthroughCompleted),
       routes: {
-        '/walkthrough': (context) => const Walkthrough(), // Single walkthrough route
-        '/': (context) => ScreenWithAppBar(child: LoginScreen()),
+        '/walkthrough': (context) => const Walkthrough(),
         '/signup': (context) => ScreenWithAppBar(child: RegisterScreen()),
         '/main': (context) => ScreenWithAppBar(child: UserScreen()),
       },
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  final bool walkthroughCompleted;
+
+  const SplashScreen({super.key, required this.walkthroughCompleted});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Container(
+        height: double.infinity,
+        width: double.infinity,
+          color: Colors.white,
+        child: Image.asset("assets/icons/logo.png", width: 500,),
+      ),
+      nextScreen: walkthroughCompleted ? LoginScreen() : Walkthrough(),
+      splashTransition: SplashTransition.fadeTransition,
+      duration: 2000,
     );
   }
 }
@@ -53,10 +75,11 @@ class ScreenWithAppBar extends StatelessWidget {
   final Widget child;
   final Color backgroundColor;
 
-  ScreenWithAppBar(
-      {this.title,
-      required this.child,
-      this.backgroundColor = Colors.transparent});
+  const ScreenWithAppBar({
+    this.title,
+    required this.child,
+    this.backgroundColor = Colors.transparent,
+  });
 
   @override
   Widget build(BuildContext context) {
